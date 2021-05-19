4b289628ea68281856b46c1f7f19ee95f598e308
Joseph Curto <jcurto@joe.vr.it>
2021-05-19 10:56:00 +0200

    Rientro in GitHub
    GitLab non va bene per me

---
+++ Password_Generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#  Why importin tons of functions when I can import the only 2 I need?
+
+from sys import exit
+from random import choice
+
+#
+# Using try - except trap to verify if input data is a number
+# if no exit with errcode -9
+#
+
+try:
+    password_length=int(input('\nDigit the lentgh of password to generate: '))
+except:
+    print("\nA number whas required!!\n")
+    exit(-9)
+
+#
+# using a static string avoids loading unnecessary libraries at this point (like import string)
+#
+#password_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+password_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789|#$%&*+-.@'
+
+for k in range(10)
+    password = []
+
+    for x in range(password_length):
+        password.append(choice(password_characters))
+#
+# using the f-string is more python in printing in version 3
+#
+    print(f"\n{''.join(password)} \n")
---
+++ README.md
@@ -0,0 +1,8 @@
+# Password Generator
+***
+Lo script, data una stringa contenente i caratteri da usare per creare la password, chiede la lunghezza della password da generare.
+
+- In un caso crea una sola password.
+- Nell'altro genera una lista di 10 password diverse della lunghezza richiesta.
+
+###### Aggiornato il 24/03/2021 - 10:58
